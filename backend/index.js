050a6160fe7febe852334452d2ed7fd8bad7cc8d
Shailendra pathak <pathakharsh813@gmail.com>
2025-02-17 14:38:03 +0530

    user and address model created

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(morgan());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+
+connectDB().then(() => {  
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+})
+});
