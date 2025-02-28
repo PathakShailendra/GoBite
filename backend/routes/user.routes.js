1e4c2cdb3b36ed659a327b9f8dc81bd95b26881c
Shailendra pathak <pathakharsh813@gmail.com>
2025-02-28 10:36:20 +0530

    forget password Api added successfully

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { forgetPasswordController, loginController, logoutController, registerUserController, updateUserDetails, uploadAvatar, verifyEmailController } from "../controllers/user.controller.js";
+import { forgetPasswordController, loginController, logoutController, registerUserController, resetPassword, updateUserDetails, uploadAvatar, verifyEmailController, verifyForgotPasswordOtp } from "../controllers/user.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import upload from "../middleware/multer.js";
 const router = Router();
@@ -11,5 +11,7 @@ router.get('/logout', authMiddleware,  logoutController)
 router.put('/upload-avatar', authMiddleware, upload.single('avatar') ,uploadAvatar)
 router.put('/update-user', authMiddleware, updateUserDetails)
 router.put('/forgot-password', forgetPasswordController)
+router.put('/verify-forgot-password', verifyForgotPasswordOtp)
+router.put('/reset-password', resetPassword)
 
 export default router;
