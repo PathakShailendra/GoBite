15617e5075e61320ba6f88370cd11ecc1522fa67
Shailendra pathak <pathakharsh813@gmail.com>
2025-03-01 11:26:15 +0530

    Login and Add to Cart button added to NavBar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,49 +1,77 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import Search from "./Search";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaRegCircleUser } from "react-icons/fa6";
+import useMobile from "../hooks/useMobile";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Header = () => {
+  const [isMobile] = useMobile();
+
+  const location = useLocation();
+
+  const isSearchPage = location.pathname === "/search";
+  const navigate = useNavigate();
+  const redirectToLoginPage = () => {
+    navigate("/login");
+  }
+
   return (
     <header className="h-24 lg:h-20 sticky lg:shadow-md top-0 flex justify-center flex-col gap-1">
-      <div className="container mx-auto flex items-center px-2 justify-between">
-        {/* logo */}
-        <div className="h-full">
-          <Link to={"/"} className="h-full flex items-center justify-center">
-            <img
-              src={logo}
-              width={170}
-              height={60}
-              alt="logo"
-              className="hidden lg:block"
-            />
-            <img
-              src={logo}
-              width={110}
-              height={60}
-              alt="logo"
-              className="lg:hidden"
-            />
-          </Link>
-        </div>
+      {!(isSearchPage && isMobile) && (
+        <div className="container mx-auto flex items-center px-2 justify-between">
+          {/* logo */}
+          <div className="h-full">
+            <Link to={"/"} className="h-full flex items-center justify-center">
+              <img
+                src={logo}
+                width={170}
+                height={60}
+                alt="logo"
+                className="hidden lg:block"
+              />
+              <img
+                src={logo}
+                width={110}
+                height={60}
+                alt="logo"
+                className="lg:hidden"
+              />
+            </Link>
+          </div>
 
-        {/* search */}
+          {/* search */}
 
-        <div className="hidden lg:block">
-          <Search />
-        </div>
-
-        {/* login and my cart */}
-        <div>
-          <button className="text-neutral-600 lg:hidden">
-          <FaRegCircleUser size={25} />
-          </button>
           <div className="hidden lg:block">
-            Login my cart
+            <Search />
+          </div>
+
+          {/* login and my cart */}
+          <div>
+            {/**user icons display in only mobile version**/}
+            <button className="text-neutral-600 lg:hidden">
+              <FaRegCircleUser size={25} />
+            </button>
+
+
+            {/**Desktop**/}
+            <div className="hidden lg:flex items-center gap-10">
+              <button onClick={redirectToLoginPage} className="text-lg px-2 font-semibold">Login</button>
+              <button className="flex items-center gap-2 bg-green-800 hover:bg-green-700 px-3 py-3 text-white rounded-lg">
+                {/* {add to cart icons} */}
+                <div className="animate-bounce">
+                <FaCartShopping  size={28} />
+                </div>
+                <div className="font-semibold">
+                  <p> My cart </p>
+                </div>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+
       <div className="container mx-auto px-2 lg:hidden">
         <Search />
       </div>
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
+import { FaArrowLeft } from "react-icons/fa";
+import useMobile from "../hooks/useMobile";
 
 const Search = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [isSearchPage, setIsSearchPage] = useState(false);
+  const [isMobile] = useMobile();
+
   useEffect(() => {
     const isSearch = location.pathname === "/search";
     setIsSearchPage(isSearch);
@@ -17,9 +21,17 @@ const Search = () => {
 
   return (
     <div className="w-full min-w-[300px] lg:min-w-[420px] h-10 lg:h-12 rounded-lg border  overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-[#ffbf00]">
-      <button className="flex justify-center items-center h-full p-3 group-focus-within:text-[#ffbf00]">
-        <IoSearch size={22} />
-      </button>
+      <div>
+        {isMobile && isSearchPage ? (
+          <Link to={"/"} className="flex justify-center items-center h-full p-2 m-1 group-focus-within:text-[#ffbf00] bg-white rounded-full shadow-md">
+            <FaArrowLeft />
+          </Link>
+        ) : (
+          <button className="flex justify-center items-center h-full p-3 group-focus-within:text-[#ffbf00]">
+            <IoSearch size={22} />
+          </button>
+        )}
+      </div>
       <div className="w-full h-full">
         {!isSearchPage ? (
           // not in search page
