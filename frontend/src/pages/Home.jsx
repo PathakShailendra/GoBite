c098271aee78355803a9c94143ee9fcfca302a03
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-01 01:35:09 +0530

    both large and mobile banner updated successfully

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import banner from "../assets/banner.jpg";
+import bannerMobile from '../assets/banner-mobile.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="bg-white">
+      <div className="container mx-auto">
+        <div
+          className={`w-full h-full min-h-48 bg-blue-100 rounded 
+          ${!banner && "animate-pulse my-2"}`}
+        >
+          <img src={banner} className="w-full h-full hidden lg:block" alt="banner" />
+          <img src={bannerMobile} className="w-full mt-10 h-full lg:hidden" alt="banner" />
+        </div>
+      </div>
 
-export default Home
+      
+    </section>
+  );
+};
+
+export default Home;
