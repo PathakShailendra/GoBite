60e759d4c448998ffe97529422e46b9b59b23014
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-02 15:12:42 +0530

    getProductByCategoryAndSubCategory added successfully

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
-import { createProductController, getProductByCAtegory, getProductController } from "../controllers/product.controller.js";
+import { createProductController, getProductByCAtegory, getProductByCategoryAndSubCategory, getProductController } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.post("/create", authMiddleware, createProductController);
 router.post('/get', getProductController);
 router.post('/get-product-by-category', getProductByCAtegory)
+router.post('/get-product-by-category-and-subcategory', getProductByCategoryAndSubCategory)
 
 
 
