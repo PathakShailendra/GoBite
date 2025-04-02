f625b8eb9815cd285f7d08ecdd5bfeb369d9aa2a
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-02 14:53:37 +0530

    Building ProductListPage

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductListPage = () => {
+  const params = useParams();
+  console.log(params)
   return (
-    <div>ProductListPage</div>
+    <section className='mt-7 lg:mt-0 sticky top-24 lg:top-20'>
+      <div className='container sticky mx-auto grid grid-cols-[90px_1fr] md:grid-cols-[200px_1fr] lg:grid-cols-[280px_1fr]'>
+        {/* Sub category */}
+        <div className='min-h-[80vh] bg-red-300'>
+          Sub Category
+        </div>
+
+
+        {/* Product */}
+        <div className='bg-green-300'>
+          Product
+        </div>
+      </div>
+    </section>
   )
 }
 
