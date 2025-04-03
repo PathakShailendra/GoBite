ed966c676228dfb65ab3ef29cb2eec09348f5b9a
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-04 01:46:53 +0530

    Building productDisplaypage

--- frontend/src/pages/ProductDisplayPage.jsx
+++ frontend/src/pages/ProductDisplayPage.jsx
@@ -1,14 +1,89 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
+import React, { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import SummaryApi from "../common/SummaryApi";
+import Axios from "../utils/Axios";
+import AxiosToastError from "../utils/AxiosToastError";
 
 const ProductDisplayPage = () => {
   const params = useParams();
-  let productId = params?.product?.split("-")?.slice(-1)[0]
+  let productId = params?.product?.split("-")?.slice(-1)[0];
+  const [data, setData] = useState({
+    name: "",
+    image: [],
+  });
+  const [image, setImage] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const imageContainer = useRef();
 
-  console.log(productId)
+  const fetchProductDetails = async () => {
+    try {
+      const response = await Axios({
+        ...SummaryApi.getProductDetails,
+        data: {
+          productId: productId,
+        },
+      });
+
+      const { data: responseData } = response;
+      // console.log(responseData)
+      if (responseData.success) {
+        setData(responseData.data);
+      }
+    } catch (error) {
+      AxiosToastError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductDetails();
+  }, [params]);
+
   return (
-    <div>ProductDisplayPage</div>
-  )
-}
+    <section className="container mx-auto p-4 grid lg:grid-cols-2">
+      <div className=" ">
+        <div className=" lg:min-h-[65vh] lg:max-h-[65vh] rounded min-h-52 max-h-52 h-full w-full">
+          <img
+            src={data.image[image]}
+            alt=""
+            className="w-full h-full object-scale-down"
+          />
+        </div>
+        <div className="flex items-center justify-center gap-3 my-2">
+          {data.image.map((img, index) => {
+            return (
+              <div
+                key={img + index + "point"}
+                className={`bg-slate-200 w-3 h-3 lg:w-5 lg:h-5 rounded-full ${
+                  index === image && "bg-slate-400"
+                }`}
+              ></div>
+            );
+          })}
+        </div>
+        <div className='grid relative'>
+          <div className='flex gap-4 z-10 relative w-full overflow-x-auto scrollbar-none'>
+            {data.image.map((img, index) => {
+              return (
+                <div
+                  className="w-20 h-20 min-h-20 min-w-20 scr cursor-pointer shadow-md"
+                  key={img + index}
+                >
+                  <img
+                    src={img}
+                    alt="min-product"
+                    onClick={() => setImage(index)}
+                    className="w-full h-full object-scale-down"
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default ProductDisplayPage
+export default ProductDisplayPage;
