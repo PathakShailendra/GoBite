f088079a24ebc178f7d1c625d92ab56793dee7d8
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-05 00:24:17 +0530

    Building product page

--- frontend/src/pages/ProductDisplayPage.jsx
+++ frontend/src/pages/ProductDisplayPage.jsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import SummaryApi from "../common/SummaryApi";
 import Axios from "../utils/Axios";
 import AxiosToastError from "../utils/AxiosToastError";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
+import { DisplayPriceInRupees } from "../utils/DisplayPriceInRupees";
+import Divider from "../components/Divider";
 
 const ProductDisplayPage = () => {
   const params = useParams();
@@ -40,8 +43,15 @@ const ProductDisplayPage = () => {
     fetchProductDetails();
   }, [params]);
 
+  const handleScrollRight = () => {
+    imageContainer.current.scrollLeft += 100;
+  };
+  const handleScrollLeft = () => {
+    imageContainer.current.scrollLeft -= 100;
+  };
+
   return (
-    <section className="container mx-auto p-4 grid lg:grid-cols-2">
+    <section className="container mx-auto mt-4 p-4 grid lg:grid-cols-2">
       <div className=" ">
         <div className=" lg:min-h-[65vh] lg:max-h-[65vh] rounded min-h-52 max-h-52 h-full w-full">
           <img
@@ -62,8 +72,11 @@ const ProductDisplayPage = () => {
             );
           })}
         </div>
-        <div className='grid relative'>
-          <div className='flex gap-4 z-10 relative w-full overflow-x-auto scrollbar-none'>
+        <div className="grid relative">
+          <div
+            ref={imageContainer}
+            className="flex gap-4 z-10 relative w-full overflow-x-auto scrollbar-none items-center"
+          >
             {data.image.map((img, index) => {
               return (
                 <div
@@ -80,7 +93,42 @@ const ProductDisplayPage = () => {
               );
             })}
           </div>
+
+          <div className="w-full flex justify-between absolute h-full items-center -ml-7">
+            <button
+              onClick={handleScrollLeft}
+              className=" z-10 bg-white p-1 rounded-full shadow-lg"
+            >
+              <FaAngleLeft />
+            </button>
+
+            <button
+              onClick={handleScrollRight}
+              className="z-10 bg-white p-1 rounded-full shadow-lg"
+            >
+              <FaAngleRight />
+            </button>
+          </div>
         </div>
+      </div>
+
+      <div className="p-4 lg:pl-7 text-base lg:text-lg">
+        <p className="bg-green-300 w-fit px-2 rounded-full mb-1">10 min</p>
+        <h2 className="text-lg font-semibold lg:text-3xl">{data.name}</h2>
+        <p className="">{data.unit}</p>
+        <Divider />
+        <div>
+          <p className="">Price</p>
+          <div className="border border-green-600 px-4 py-2 rounded bg-green-50 w-fit">
+            <p className="font-semibold text-lg lg:text-xl">{DisplayPriceInRupees(data.price)}</p>
+          </div>
+        </div>
+
+        <button className="my-4 px-4 py-1 bg-green-700 hover:bg-green-700 text-white rounded shadow-md">
+          Add
+        </button>
+
+        <Divider />
       </div>
     </section>
   );
