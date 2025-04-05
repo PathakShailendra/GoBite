1224044466ae7d0d796d801743da1786bd0dc1d0
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-05 13:20:34 +0530

    Search product controller added

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -239,32 +239,80 @@ export const updateProductDetails = async (req, res) => {
   }
 };
 
-
-export const deleteProductDetails = async(req,res)=>{
+export const deleteProductDetails = async (req, res) => {
   try {
-      const { _id } = req.body 
+    const { _id } = req.body;
 
-      if(!_id){
-          return res.status(400).json({
-              message : "provide _id ",
-              error : true,
-              success : false
-          })
-      }
+    if (!_id) {
+      return res.status(400).json({
+        message: "provide _id ",
+        error: true,
+        success: false,
+      });
+    }
 
-      const deleteProduct = await ProductModel.deleteOne({_id : _id })
+    const deleteProduct = await ProductModel.deleteOne({ _id: _id });
 
-      return res.status(200).json({
-          message : "Delete successfully",
-          error : false,
-          success : true,
-          data : deleteProduct
-      })
+    return res.status(200).json({
+      message: "Delete successfully",
+      error: false,
+      success: true,
+      data: deleteProduct,
+    });
   } catch (error) {
-      return res.status(500).json({
-          message : error.message || error,
-          error : true,
-          success : false
-      })
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
   }
-}
+};
+
+export const searchProduct = async (req, res) => {
+  try {
+    let { search, page, limit } = req.body;
+
+    if (!page) {
+      page = 1;
+    }
+    if (!limit) {
+      limit = 10;
+    }
+
+    const query = search
+      ? {
+          $text: {
+            $search: search,
+          },
+        }
+      : {};
+
+    const skip = (page - 1) * limit;
+
+    const [data, dataCount] = await Promise.all([
+      ProductModel.find(query)
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
+        .populate("category subCategory"),
+      ProductModel.countDocuments(query),
+    ]);
+
+    return res.json({
+      message: "Product data",
+      error: false,
+      success: true,
+      data: data,
+      totalCount: dataCount,
+      totalPage: Math.ceil(dataCount / limit),
+      page: page,
+      limit: limit,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CardLoading from '../components/CardLoading'
 
 const SearchPage = () => {
+  const [data, setData ] = useState([])
+  const [loading, setLoading] = useState(true)
+  const loadingarrayCard = new Array(10).fill(null)
   return (
-    <div>SearchPage</div>
+    <section className='bg-white'>
+      <div className='container mx-auto p-4'> 
+        <p>Search Result : {data.length}</p>
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 py-4'>
+        
+
+
+        {/* Loading data */}
+        {
+          loading && (
+            loadingarrayCard.map((_, index) => {
+              return (
+                <CardLoading key={"loadingSearchPage"+index} />
+              )
+            })
+          )
+        }
+        </div>
+      </div>
+    </section>
   )
 }
 
