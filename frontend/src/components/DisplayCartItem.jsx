e16be79461b370af4bd3c965ce5044180e7e2ff6
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-13 12:57:59 +0530

    Cart products ui update

--- frontend/src/components/DisplayCartItem.jsx
+++ frontend/src/components/DisplayCartItem.jsx
@@ -10,8 +10,7 @@ import AddToCartButton from "./AddToCartButton";
 
 const DisplayCartItem = ({ close }) => {
   const { notDiscountTotalPrice, totalPrice, totalQty } = useGlobalContext();
-  const cartItem  = useSelector(state => state.cartItem.cart)
-  
+  const cartItem = useSelector((state) => state.cartItem.cart);
 
   return (
     <section className="bg-neutral-900/70 fixed top-0 left-0 right-0 bottom-0 z-50">
@@ -19,10 +18,7 @@ const DisplayCartItem = ({ close }) => {
         <div className="flex items-center justify-between p-4 shadow-md">
           <h2 className="font-semibold">Cart</h2>
           <Link to={"/"} className="lg:hidden">
-            <IoClose
-              size={25}
-              className="text-gray-700 transition-colors duration-300 hover:text-red-500"
-            />
+            <IoClose size={25} />
           </Link>
           <button
             onClick={close}
@@ -35,45 +31,56 @@ const DisplayCartItem = ({ close }) => {
           </button>
         </div>
 
-        <div className="min-h-[75vh] lg:min-h-[80vh] h-full max-h-[calc(100vh-150px)] bg-blue-50 p-2 flex flex-col gap-4">
+        <div className="min-h-[75vh] lg:min-h-[80vh] h-full max-h-[calc(100vh-150px)] bg-blue-50 p-4 flex flex-col gap-4">
           {/* Display items */}
 
-          <div className="flex items-center justify-between px-4 py-2 bg-blue-100 text-blue-500 rounded-full">
+          <div className="flex items-center justify-between px-5 py-3 bg-blue-100 text-blue-500 rounded-xl shadow-sm text-sm font-medium">
             <p>Your total savings</p>
             <p>{DisplayPriceInRupees(notDiscountTotalPrice - totalPrice)}</p>
           </div>
 
-          <div className="bg-white rounded-lg p-4 grid gap-5 overflow-auto">
-            {
-                cartItem[0] && (
-                    cartItem.map((item,index)=>{
-                        return(
-                            <div key={item?._id+"cartItemDisplay"} className='flex  w-full gap-4'>
-                                <div className='w-16 h-16 min-h-16 min-w-16 bg-red-500 border rounded'>
-                                    <img
-                                        src={item?.productId?.image[0]}
-                                        className='object-scale-down'
-                                    />
-                                </div>
-                                <div className='w-full max-w-sm text-xs'>
-                                    <p className='text-xs text-ellipsis line-clamp-2'>{item?.productId?.name}</p>
-                                    <p className='text-neutral-400'>{item?.productId?.unit}</p>
-                                    <p className='font-semibold'>{DisplayPriceInRupees(pricewithDiscount(item?.productId?.price,item?.productId?.discount))}</p>
-                                </div>
-                                <div className="h-10">
-                                    <AddToCartButton variant={"card"} data={item?.productId}/>
-                                </div>
-                            </div>
-                        )
+          <div className="bg-white rounded-xl p-4 grid gap-6 overflow-auto shadow-md">
+            {cartItem[0] &&
+              cartItem.map((item, index) => {
+                return (
+                  <div
+                    key={item?._id + "cartItemDisplay"}
+                    className="flex items-center w-full gap-4"
+                  >
+                    <div className="w-16 h-16 min-w-16 min-h-16 bg-white border border-gray-200 rounded-md overflow-hidden flex items-center justify-center shadow-sm">
+                      <img
+                        src={item?.productId?.image[0]}
+                        className="object-contain h-full w-full"
+                      />
+                    </div>
 
-                    })
-                )
-            }
+                    <div className="w-full max-w-sm text-xs space-y-1">
+                      <p className="text-sm font-medium text-gray-800 line-clamp-2">
+                        {item?.productId?.name}
+                      </p>
+                      <p className="text-neutral-400 text-xs">
+                        {item?.productId?.unit}
+                      </p>
+                      <p className="font-semibold text-green-700">
+                        {DisplayPriceInRupees(
+                          pricewithDiscount(
+                            item?.productId?.price,
+                            item?.productId?.discount
+                          )
+                        )}
+                      </p>
+                    </div>
+
+                    <div className="h-10 ml-auto">
+                      <AddToCartButton
+                        variant={"card"}
+                        data={item?.productId}
+                      />
+                    </div>
+                  </div>
+                );
+              })}
           </div>
-
-
-
-
         </div>
 
         <div className="p-4">
