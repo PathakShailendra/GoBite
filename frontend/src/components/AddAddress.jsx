732df36eccd54952f4f18ba5568b27ce97f62243
Shailendra pathak <pathakharsh813@gmail.com>
2025-04-21 00:14:20 +0530

    add address page ui created

--- frontend/src/components/AddAddress.jsx
+++ frontend/src/components/AddAddress.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { IoClose } from "react-icons/io5";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 // import { useForm, SubmitHandler } from "react-hook-form";
 
-const AddAddress = () => {
+const AddAddress = ({close}) => {
   const { register, handleSubmit, reset } = useForm();
-  const onSubmit = async(data)=>{
-    console.log("data",data)
-  }
+  const onSubmit = async (data) => {
+    console.log("data", data);
+  };
 
   return (
     <section className="bg-black/70 fixed top-0 left-0 right-0 bottom-0 z-50 h-screen overflow-auto">
@@ -19,6 +19,7 @@ const AddAddress = () => {
             className="w-fit block ml-auto p-1.5 rounded-full transition-all duration-300 ease-in-out hover:bg-gray-200/50 hover:scale-110 hover:shadow-md active:scale-95 cursor-pointer"
           >
             <IoClose
+            onClick={close}
               size={25}
               className="text-gray-700 transition-colors duration-300 hover:text-red-500"
             />
@@ -32,9 +33,58 @@ const AddAddress = () => {
               type="text"
               id="addressline"
               className="border bg-blue-50 p-2 rounded"
-              {...register("addressline",{required : true})}
+              {...register("addressline", { required: true })}
             />
           </div>
+          <div className="grid gap-1">
+            <label htmlFor="city">City :</label>
+            <input
+              type="text"
+              id="city"
+              className="border bg-blue-50 p-2 rounded"
+              {...register("city", { required: true })}
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="state">State :</label>
+            <input
+              type="text"
+              id="state"
+              className="border bg-blue-50 p-2 rounded"
+              {...register("state", { required: true })}
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="pincode">Pincode :</label>
+            <input
+              type="text"
+              id="pincode"
+              className="border bg-blue-50 p-2 rounded"
+              {...register("pincode", { required: true })}
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="country">Country :</label>
+            <input
+              type="text"
+              id="country"
+              className="border bg-blue-50 p-2 rounded"
+              {...register("country", { required: true })}
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="mobile">Mobile No. :</label>
+            <input
+              type="text"
+              id="mobile"
+              className="border bg-blue-50 p-2 rounded"
+              {...register("mobile", { required: true })}
+            />
+          </div>
+
+          <button className="mt-4 px-6 py-3 font-semibold text-white bg-gradient-to-r from-[#D69CAA] to-[#C08497] rounded-lg shadow-lg transition-all duration-500 ease-in-out hover:from-[#68AB95] hover:to-[#4C9C84] hover:shadow-xl active:scale-95">
+            Submit
+          </button>
         </form>
       </div>
     </section>
